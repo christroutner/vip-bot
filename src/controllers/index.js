c6a7734b491285a29450b71cb4cae25a16f08871
Chris Troutner <chris.troutner@gmail.com>
2022-06-10 11:52:41 -0700

    fix(controllers): Adding boilerplate for timer-based controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -15,10 +15,14 @@ const UseCases = require('../use-cases')
 // Load the REST API Controllers.
 const RESTControllers = require('./rest-api')
 
+// Timer-based controllers.
+const TimerControllers = require('./timer-controllers')
+
 class Controllers {
   constructor (localConfig = {}) {
     this.adapters = new Adapters()
     this.useCases = new UseCases({ adapters: this.adapters })
+    this.timerControllers = new TimerControllers({ adapters: this.adapters, useCases: this.useCases })
   }
 
   // Spin up any adapter libraries that have async startup needs.
